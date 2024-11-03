9e791f1450b41ecefa4f74e6538942f2c778ba3b
Trần Thế Vỹ <tranthevy@David.local>
2024-11-03 23:14:40 +0700

    async await

--- request.js
+++ request.js
@@ -1,8 +1,9 @@
 class model {
   async model(req, res) {
     try {
-        const data = await req.body;
+        const data = await req.body.name;
     res.json(data);
+    res.render("index",data)
     } catch (error) {
     }
   }
